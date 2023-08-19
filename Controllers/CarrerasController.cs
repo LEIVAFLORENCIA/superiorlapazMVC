1fc32375a341acc1d5cb322d77fc222fb1049744
Florencia Leiva <leivaflorenciad@gmail.com>
2023-08-19 15:26:35 -0300

    Add project files.

---
+++ Controllers/CarrerasController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ejemploClase2.Model;
+
+namespace ejemploClase2.Controllers
+{
+    [ApiController]
+    [Route("gestorMaterias")]
+    public class CarrerasController : ControllerBase
+    {
+        List<Carrera> listCarreras = new List<Carrera>();
+        public CarrerasController() {
+            Carrera carrera1 = new Carrera(1, "Analista de Sistemas", 3);
+            Carrera carrera2 = new Carrera(2, "Profesorado de Inglés", 4);
+            listCarreras.Add(carrera1);
+            listCarreras.Add(carrera2);
+        }
+
+        [HttpGet]
+        [Route("materias")]
+        public dynamic materias()
+        {
+            return listCarreras;
+        }
+
+        [HttpPost]
+        [Route("crearMateria")]
+        public dynamic agregarMaterias(List<Carrera> listaNueva)
+        {
+            foreach (Carrera materia in listaNueva)
+            {
+                listCarreras.Add(materia);
+            }
+
+            return listCarreras;
+        }
+    }
+}
---
+++ Model/Carrera.cs
@@ -0,0 +1,16 @@
+﻿namespace ejemploClase2.Model
+{
+    public class Carrera
+    {
+        public int Id { get; set; } 
+        public string Nombre { get; set; }
+        public int Duracion { get; set; }
+
+        public Carrera(int id, string nombre, int duracion)
+        {
+            Id = id;
+            Nombre = nombre;
+            Duracion = duracion;
+        }
+    }
+}
